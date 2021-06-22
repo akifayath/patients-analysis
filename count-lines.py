d524de86e95c2cdc4590ca1dde228aaa62d42eef
akifayath <kifayath.buet@gmail.com>
2021-06-22 13:47:55 +0200

    added documentation to count-lines.py

--- count-lines.py
+++ count-lines.py
@@ -1,4 +1,9 @@
-import sys 
+
+import sys
+
+"""This module counts the number of lines in standard input
+Input: any string from system standard input"""
+ 
 count = 0
 for line in sys.stdin:
     count += 1
